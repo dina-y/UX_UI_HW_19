63601c018a526361916f95d032902155485a66a6
dina-y <yassindina4@gmail.com>
2022-12-13 15:50:06 +0200

    Added UI interaction and feedback iterations

---
+++ main.js
@@ -0,0 +1,27 @@
+// Scroll Animation
+const sr = ScrollReveal({
+    origin: 'top',
+    distance:'85px',
+    duration:2000,
+    reset: true
+})
+
+sr.reveal ('.home-text',{})
+sr.reveal ('.home-img',{delay:200})
+sr.reveal ('.social',{delay:200})
+
+sr.reveal ('.about-img',{})
+sr.reveal ('.heading',{})
+sr.reveal ('.about-text',{delay:200})
+
+sr.reveal ('.skills-content',{})
+
+sr.reveal ('.work-text',{})
+sr.reveal ('.work-img',{delay:200})
+
+
+sr.reveal ('.app h1',{})
+sr.reveal ('.app',{delay:200})
+
+sr.reveal ('form input',{})
+sr.reveal ('form textarea',{})
